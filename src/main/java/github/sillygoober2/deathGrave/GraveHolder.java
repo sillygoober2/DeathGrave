49469dcd94e3c8e59999c7e5e5d2ec928751eef1
silly <jamiee01206@gmail.com>
2025-08-22 01:38:04 +0100

    Removed unused imports

--- src/main/java/github/sillygoober2/deathGrave/GraveHolder.java
+++ src/main/java/github/sillygoober2/deathGrave/GraveHolder.java
@@ -1,7 +1,9 @@
 package github.sillygoober2.deathGrave;
 
+import org.bukkit.Location;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.UUID;
 
@@ -11,10 +13,7 @@ public class GraveHolder implements InventoryHolder {
     public GraveHolder(UUID graveId) {
         this.graveId = graveId;
     }
-
-    public UUID getGraveId() {
-        return graveId;
-    }
+    public UUID getGraveId() {return graveId;}
 
     @Override
     public Inventory getInventory(){
